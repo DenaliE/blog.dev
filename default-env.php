2bd3ed736b75ac5c5c5e56db7e89cac7bf79b1b3
denalie <denali.endicott@gmail.com>
2014-12-01 13:46:28 -0600

    Initial blog commit!

---
+++ default-env.php
@@ -0,0 +1,9 @@
+//template for all other environment files
+//array contains constants for db
+return array(
+    'DB_HOST' => '',
+    'DB_NAME' => '',
+    'DB_USER' => '',
+    'DB_PASS' => ''
+
+);
