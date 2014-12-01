436978d841ceea1f8657545264ff0b4fd8b03874
denalie <denali.endicott@gmail.com>
2014-12-01 14:40:25 -0600

    First commit.

--- app/routes.php
+++ app/routes.php
@@ -15,3 +15,32 @@ Route::get('/', function()
 {
 	return View::make('hello');
 });
+
+Route::get('say-hello', function()
+{
+    return "Hello Codeup!";
+});
+
+//first parameter is linked to first variable, sencond to second.
+//They are linked by order, not name. Variable could be named anything.
+//$id is not used here, just shows what multiple parameters look like
+Route::get('say-hello/{name}/{id}', function($name, $id)
+{
+    if ($name == 'John'){
+        return Redirect::to('/');
+    } else {
+    return "Hello $name!";
+    }
+});
+
+Route::get('resume', function()
+{
+    return "This is my resume.";
+});
+
+Route::get('portfolio', function()
+{
+    return "This is my portfolio.";
+});
+
+
