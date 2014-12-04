802a855f5adbd184c264669046466c291dc1d517
denalie <denali.endicott@gmail.com>
2014-12-04 11:37:15 -0600

    Resubmitting overwritten files to remote personal branch. Edit messages display in view.

--- app/controllers/PostsController.php
+++ app/controllers/PostsController.php
@@ -34,18 +34,23 @@ class PostsController extends \BaseController {
 	 * @return Response
 	 */
 	public function store()
-	{
-		//dd(Input::all());
-		//dd is dump and die. stops running after dump is executed
-		//return Redirect::back()->withInput();
-
-		$post = new Post();
-		$post->title = Input::get('title');
-		$post->body = Input::get('body');
-		$post->save();
-
-		return Redirect::action('PostsController@index')->withInput();
-	}
+		{
+			// create the validator
+			$validator = Validator::make(Input::all(), Post::$rules);
+			 // attempt validation
+		    if ($validator->fails()) {
+		        // validation failed, redirect to the post create page with validation errors and old inputs
+		        return Redirect::back()->withInput()->withErrors($validator);
+		    } else {
+		        // validation succeeded, create and save the post
+	        	$post = new Post();
+	        	$post->title = Input::get('title');
+	        	$post->body = Input::get('body');
+	        	$post->save();
+		        //return Redirect::action('PostsController@index')->withInput();
+		        return Redirect::action('PostsController@show', $post->id);
+		        }
+		}//end store
 
 
 	/**
